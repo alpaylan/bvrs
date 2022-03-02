e026623f59bb029f741584f528e6ea0f67807aae
alpaylan <alpkeles99@gmail.com>
2022-03-01 21:21:27 -0500

    project structure changed to library

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod bit_vec;
+mod rank_support;
+mod select_support;
+mod sparse_array;
+
+pub use bit_vec::BitVec;
+pub use rank_support::RankSupport;
+pub use select_support::SelectSupport;
+pub use sparse_array::SparseArray;
