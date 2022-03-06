d28be9a50323618119c2813a1955b2dc7df0f743
alpaylan <alpkeles99@gmail.com>
2022-03-06 15:34:56 -0500

    delete the fib from librs

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,3 @@ pub use bit_vec::BitVec;
 pub use rank_support::RankSupport;
 pub use select_support::SelectSupport;
 pub use sparse_array::SparseArray;
-
-pub fn fibonacci(n: u32) -> u32 {
-    if n <= 1 {
-        1
-    } else {
-        fibonacci(n - 1) + fibonacci(n - 2)
-    }
-}
